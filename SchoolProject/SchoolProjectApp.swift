9b6eded2705d78d503dfb3347cc5dfb00252f175
VasyaSmetankin <medvedevkon70@gmail.com>
2023-09-04 15:17:58 +0300

    Initial Commit

---
+++ SchoolProject/SchoolProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolProjectApp.swift
+//  SchoolProject
+//
+//  Created by Vasya Smetankin on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SchoolProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
